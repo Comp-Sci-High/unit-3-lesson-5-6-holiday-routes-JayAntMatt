05c959485505183070e53b97cd15015771b53c87
JayAntMatt <jaylen.matthews26@compscihigh.org>
2026-01-06 04:24:14 +0000

    finished project

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 })
 
+//A route handler that gets the summons page information, interaction,  etc. This allows the client to click on a button bringing them to a different page
+app.get("/summon", (req, res) => {
+  res.sendFile(__dirname + "/public/summon.html")
+})
+
 app.listen(3000, () => {
   console.log(`Holiday Server is Running!`)
 });
